ba6b5d3cb9511e13e5f88cd9c12985dacd88f8f1
FMDEMOLABS <108448313+fmazlabs@users.noreply.github.com>
2022-11-11 17:00:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,65 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+variable "countt" {
+  type = number
 }
 
-variable "access_key" {}
-variable "secret_key" {}
-
-#secret_key = "${var.secret_key}"
-
-provider "aws" {
-  #access_key = "<+secrets.getValue("awssecretkey")>"
-  #secret_key = "<+secrets.getValue("awssecretkeytf")>"
-  access_key = "${var.secret_key}"
-  secret_key = "${var.access_key}"
-  region = "us-east-1"
-}
-/*
-resource "aws_dynamodb_table" "terraform_locks" {
-  hash_key = "LockID"
-  name = "terraform-test-locks"
-  #billing_mode = "PAY_PER_REQUEST"
-  attribute {
-    name = "LockID"
-    type = "S"
+resource "null_resource" "example" {
+  count = "${var.countt}"
+  provisioner "local-exec" {
+    command = "echo testlocallocaltesttestlocallocaltes"
   }
 }
-*/
-resource "aws_s3_bucket" "terraform-state" {
-  bucket = "demo-tf-state-s3-bucket"
-  acl = "private"
-  versioning {
-    enabled = true
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-/*
-  server_side_encryption_configuration {
-    rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
-    }
-  }
-
-}
-
-terraform {
-  backend "s3" {
-    bucket = "demo-tf-state-s3-bucket"
-    key = "moe/s3/terraform.tfstate"
-    region = "us-east-1"
-
-    # DynamoDB table name
-    dynamodb_table = "terraform-test-locks"
-    encrypt = true
-  }
-}
-*/
