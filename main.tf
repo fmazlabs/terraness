6f6389bc36e3fefcea5c6603cf4b6ed9d49d45e4
FMDEMOLABS <108448313+fmazlabs@users.noreply.github.com>
2022-11-11 17:27:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,17 +26,9 @@ provider "aws" {
   secret_key = "${var.access_key}"
   region = "us-east-1"
 }
-/*
-resource "aws_dynamodb_table" "terraform_locks" {
-  hash_key = "LockID"
-  name = "terraform-test-locks"
-  #billing_mode = "PAY_PER_REQUEST"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-*/
+
+
+
 resource "aws_s3_bucket" "terraform-state" {
   bucket = "demo-tf-state-s3-bucket"
   acl = "private"
@@ -47,26 +39,5 @@ resource "aws_s3_bucket" "terraform-state" {
     prevent_destroy = true
   }
 }
-/*
-  server_side_encryption_configuration {
-    rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
-    }
-  }
 
-}
-
-terraform {
-  backend "s3" {
-    bucket = "demo-tf-state-s3-bucket"
-    key = "moe/s3/terraform.tfstate"
-    region = "us-east-1"
-
-    # DynamoDB table name
-    dynamodb_table = "terraform-test-locks"
-    encrypt = true
-  }
-}
-*/
+ 
