6c21d978c6e7da09d1e4559fc96f65d43c7afda4
FMDEMOLABS <108448313+fmazlabs@users.noreply.github.com>
2022-11-11 17:44:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ variable "secret_key" {
   type = string
 }
 
-#terraform {
- # required_providers {
-  #  aws = {
-   #   source  = "hashicorp/aws"
-    #  version = "~> 4.0"
-   # }
-  #}
-#}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
 
 
