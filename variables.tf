c92ef3887983c7f61f325d3da34b6c684a4ed2d6
FMDEMOLABS <108448313+fmazlabs@users.noreply.github.com>
2022-11-11 16:01:54 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  type        = string
+  default     = ""
+ # description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "secret_key" {
+  type        = string
+  default     = ""
+ # description = "Name  (e.g. `app` or `cluster`)."
+}
